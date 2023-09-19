9fd3bcfb780f72db08aff728f0efe9a6f43e27b7
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-09-19 15:19:44 +0900

    Fix: CORS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const scheduleRouter = require("./routes/schedule");
+const schedulesRouter = require("./routes/schedules");
 const db = require("./models");
+const cors = require("cors");
 const app = express();
 
 db.sequelize
@@ -10,14 +12,17 @@ db.sequelize
   })
   .catch(console.error);
 
+app.use(cors({ origin: true, credentials: true }));
+// 프론트에서 보낸 데이터를 req.body에 넣어주기위해 이 두가지 작성.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("hello express");
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use("/schedule", scheduleRouter);
+app.use("/schedules", schedulesRouter);
 
 app.listen(3065, () => {
   console.log("서버 실행중!!");
