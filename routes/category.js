f611577a44ec6dda6b731aa44e9df9f402c59a28
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-10-11 17:32:02 +0900

    Feat: 캘린더 카테고리목록 수정기능

--- routes/category.js
+++ routes/category.js
@@ -23,4 +23,28 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:categoryId", async (req, res) => {
+  try {
+    const category = await Category.findOne({
+      where: { id: req.params.categoryId },
+    });
+    if (!category) {
+      return res.status(403).send("존재하지 않는 카테고리입니다.");
+    }
+    await Category.update(
+      {
+        category: req.body.category,
+        color: req.body.color,
+      },
+      {
+        where: { id: req.params.categoryId },
+      }
+    );
+    res.status(201).send("카테고리를 변경하였습니다.");
+  } catch (err) {
+    console.error(error);
+    res.status(500).send("카테고리를 수정할수없습니다.");
+  }
+});
+
 module.exports = router;
