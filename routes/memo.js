5e4147db0817b620ac69ea8048f8d6dbbd7d6495
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-11-10 16:56:56 +0900

    Feat: 메모 조회

--- routes/memo.js
+++ routes/memo.js
@@ -2,6 +2,15 @@ const express = require("express");
 const { Memo } = require("../models");
 const router = express.Router();
 
+router.get("/:memoId", async (req, res) => {
+  try {
+    const memo = await Memo.findOne({ where: { id: req.params.memoId } });
+    res.status(200).json(memo);
+  } catch (err) {
+    res.status(500).send("메모를 조회할수없습니다.");
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     await Memo.create({
