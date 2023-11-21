656077be13e2dbaa49bd69d436e800b939555c7e
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-11-21 19:27:11 +0900

    Test: socket

--- utils/io.js
+++ utils/io.js
@@ -2,5 +2,9 @@ module.exports = function (io) {
   // io 관련된 모든일~~~ 말하는함수 emit(), 듣는함수 on()
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
+
+    socket.on("disconnect", async () => {
+      console.log("user is disconnected");
+    });
   });
 };
