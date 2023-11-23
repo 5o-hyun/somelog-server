29e56bb4cb7ac24577bd642b36ae57e450fa4872
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-11-23 15:56:44 +0900

    Feat: 메시지주고받기

--- utils/io.js
+++ utils/io.js
@@ -1,3 +1,4 @@
+const chatController = require("../controllers/chat.controller");
 const userController = require("../controllers/user.controller");
 
 module.exports = function (io) {
@@ -11,6 +12,21 @@ module.exports = function (io) {
       try {
         const user = await userController.saveUser(userName, socket.id);
         cb({ ok: true, data: user });
+      } catch (err) {
+        cb({ ok: false, error: err.message });
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        // 유저찾기 socket.id로
+        const user = await userController.checkUser(socket.id);
+        // 메세지 저장
+        const newMessage = await chatController.saveChat(message, user);
+        // 서버가 새로운메세지를 모두에게 알려야함
+        io.emit("message", newMessage);
+
+        cb({ ok: true });
       } catch (err) {
         cb({ ok: false, error: err.message });
       }
